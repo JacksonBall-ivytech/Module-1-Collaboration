87d7303819f293c3a833721359014917150db8a5
JacksonBall-ivytech <jball105@ivytech.edu>
2024-09-24 12:23:01 -0400

    Add files via upload

--- M3_case_study.py
+++ M3_case_study.py
@@ -12,4 +12,24 @@ class Automobile(Vehicle):
         self.roof = roof
     
     def describe_car(self):
-        print(f'Vehicle type: {self.type}')
+        details = (
+            f'Vehicle type: {self.type}\n'
+            f'Year: {self.year}\n'
+            f'Make: {self.make}\n'
+            f'Model: {self.model}\n'
+            f'Number of doors: {self.doors}\n'
+            f'Type of roof: {self.roof}\n'
+        )
+        print(details)
+
+vehicle_type = input('What kind of vehicle do you have?: ')
+
+if vehicle_type.lower() in ['car', 'automobile', 'motorcar', 'auto']:
+    year = input('What year is your car?: ')
+    make = input('What is the make of your car?: ')
+    model = input('What is the model of your car?: ')
+    doors = input('How many doors does it have?: ')
+    roof = input('What type of roof does it have?: ')
+    car = Automobile(vehicle_type, year, make, model, doors, roof)
+else:
+    vehicle = Vehicle(vehicle_type)
